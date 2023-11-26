dcc19ef83acc214c1242909968bf245a2b259e02
Harkay99 <106724680+Harkay99@users.noreply.github.com>
2023-11-25 22:17:33 -0500

    Update Account.java

--- src/main/java/ca/concordia/server/Account.java
+++ src/main/java/ca/concordia/server/Account.java
@@ -1,25 +1,47 @@
 package ca.concordia.server;
 
+import java.util.concurrent.atomic.AtomicInteger;
 public class Account {
-    //represent a bank account with a balance and withdraw and deposit methods
-    private int balance;
-    private int id;
+    private final AtomicInteger balance;
+    private final int id;
 
-    public Account(int balance, int id){
-
-        this.balance = balance;
-        this.id = id;
+    public Account(String line) {
+        String[] parts = line.split(",");
+        if (parts.length == 2) {
+            this.id = Integer.parseInt(parts[0].trim());
+            this.balance = new AtomicInteger(Integer.parseInt(parts[1].trim()));
+        } else {
+            throw new IllegalArgumentException("Invalid line in accounts file: " + line);
+        }
     }
 
-    public int getBalance(){
-        return balance;
+    public int getBalance() {
+        return balance.get();
     }
 
-    public void withdraw(int amount){
-        balance -= amount;
+    public int getId(){
+        return id;
     }
 
-    public void deposit(int amount){
-        balance += amount;
+    public void withdraw(int amount) {
+        int currentBalance = balance.get();
+        if (currentBalance >= amount) {
+            // Using compareAndSet to ensure atomic withdrawal
+            while (!balance.compareAndSet(currentBalance, currentBalance - amount)) {
+                currentBalance = balance.get();
+            }
+        } else {
+            throw new IllegalArgumentException("Insufficient funds");
+        }
+    }
+
+    public void deposit(int amount) {
+        // Using addAndGet to ensure atomic deposit
+        balance.addAndGet(amount);
+    }
+
+    @Override
+    public String toString() {
+        return "Account{id=" + id + ", balance=" + balance + '}';
     }
 }
